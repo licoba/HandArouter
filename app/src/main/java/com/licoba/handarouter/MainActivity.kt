bc255d24b2b78494432ed947815d63e530f4495f
licoba <licoba@qq.com>
2023-09-06 11:40:00 +0800

    1

--- app/src/main/java/com/licoba/handarouter/MainActivity.kt
+++ app/src/main/java/com/licoba/handarouter/MainActivity.kt
@@ -1,46 +1,30 @@
 package com.licoba.handarouter
 
 import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.licoba.handarouter.ui.theme.HandArouterTheme
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import com.licoba.handarouter.databinding.ActivityMainBinding
 
-class MainActivity : ComponentActivity() {
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var mBinding:ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            HandArouterTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
-            }
-        }
+        mBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(mBinding.root)
+        mBinding.initListener()
     }
-}
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    HandArouterTheme {
-        Greeting("Android")
+    private fun ActivityMainBinding.initListener(){
+        mBinding.btn1.setOnClickListener { startModule1MainActivity() }
     }
+
+
+    fun startModule1MainActivity() {
+//        EasyRouter.getsInstance().build("/module1/module1main").navigation();
+
+    }
+
 }
